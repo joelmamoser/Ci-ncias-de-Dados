21efe2fc68f7eb33e1f8de9880add00eb8ae9286
joelmamoser <134527049+joelmamoser@users.noreply.github.com>
2023-05-24 23:10:05 -0300

    Add files via upload

---
+++ CNES.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#############CELULA 1
+import re
+import ftplib
+
+DATASUS_HOST = "ftp.datasus.gov.br"
+CNES_DIR = "cnes"
+FTP_CNES_FILE = r'(BASE_DE_DADOS_CNES_)(\d{6})(\.ZIP)'
+
+
+# In[6]:
+
+
+#############CÉLULA 2
+def download_latest_cnes_dataset(temp_dir):
+    with ftplib.FTP(DATASUS_HOST) as ftp:
+        ftp.login()
+        ftp.cwd(CNES_DIR)
+
+        latest_file = [None, 0, None]
+        file_list = ftp.nlst()
+
+        for line in file_list:
+            match = re.search(FTP_CNES_FILE, line)
+            if match is not None:
+                n = match.group(1)
+                v = int(match.group(2))
+                f = match.group(3)
+
+                latest_file[0] = n if latest_file[0] is None else latest_file[0]
+                latest_file[1] = v if v > latest_file[1] else latest_file[1]
+                latest_file[2] = f if latest_file[2] is None else latest_file[2]
+
+        latest_file[1] = str(latest_file[1])
+        version = latest_file[1]
+        latest_file_str = "".join(latest_file)
+
+        handler_func = open(temp_dir + latest_file_str, 'wb').write
+        try:
+            ftp.retrbinary("RETR " + latest_file_str, handler_func)
+        except Exception as e:
+            raise Exception("retrieving archived CNES dataset: " + str(e))
+
+    return temp_dir + latest_file_str, version
+
+
+# In[10]:
+
+
+##############CÉLULA 3 - só alterar o caminho para onde você quer que ele salve o arquivo
+
+
+download_latest_cnes_dataset(r'\Users\Jo\Desktop\PÓS')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
